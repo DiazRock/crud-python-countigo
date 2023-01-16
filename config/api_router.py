de5b5816e2202bc9cb2f695a8815f26a780a76e7
DiazRock <corolariodiaz@gmail.com>
2023-01-16 08:44:47 -0500

    Some changes in the api router

--- config/api_router.py
+++ config/api_router.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-from crud.users.api.views import UserViewSet, TechnologyViewSet, TechnologyExperience
+from crud.users.api.views import UserViewSet, TechnologyViewSet, TechnologyExperienceViewSet
 
 if settings.DEBUG:
     router = DefaultRouter()
@@ -10,7 +10,7 @@ else:
 
 router.register("users", UserViewSet)
 router.register("technology", TechnologyViewSet)
-router.register("technology-experience", TechnologyExperience)
+router.register("technology-experience", TechnologyExperienceViewSet)
 
 app_name = "api"
 urlpatterns = router.urls
