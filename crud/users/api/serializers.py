3521758abb9e6225a99dad04bc01450aa1987975
DiazRock <corolariodiaz@gmail.com>
2023-01-16 00:31:15 -0500

    Added Technology and TechnologyExperience

--- crud/users/api/serializers.py
+++ crud/users/api/serializers.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from crud.users.models import Technology, TechnologyExperience
 
 User = get_user_model()
-
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -18,3 +18,19 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             "url": {"view_name": "api:user-detail", "lookup_field": "username"}
         }
+
+class TechnologySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Technology
+        fields = [
+            "name"
+        ]
+
+class TechnologyExperienceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TechnologyExperience
+        fields = [
+            "user",
+            "technology",
+            "experience"
+        ]
--- crud/users/api/views.py
+++ crud/users/api/views.py
@@ -1,19 +1,29 @@
 from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.mixins import (
+    ListModelMixin, 
+    RetrieveModelMixin, 
+    UpdateModelMixin,
+    CreateModelMixin)
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
-
-from .serializers import UserSerializer
+from crud.users.models import Technology, TechnologyExperience
+from .serializers import UserSerializer, TechnologySerializer, TechnologyExperienceSerializer
 
 User = get_user_model()
 
 
-class UserViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericViewSet):
+class UserViewSet(
+        RetrieveModelMixin, 
+        ListModelMixin, 
+        UpdateModelMixin,
+        CreateModelMixin, 
+        GenericViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
-    lookup_field = "username"
+    lookup_field = "first_name"
+    allowed_methods= ['POST', 'GET']
 
     def get_queryset(self, *args, **kwargs):
         #import ipdb; ipdb.set_trace()
@@ -24,3 +34,25 @@ class UserViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericV
     def me(self, request):
         serializer = UserSerializer(request.user, context={"request": request})
         return Response(status=status.HTTP_200_OK, data=serializer.data)
+    
+
+class TechnologyViewSet(
+    RetrieveModelMixin,
+    ListModelMixin,
+    UpdateModelMixin,
+    GenericViewSet,
+    CreateModelMixin):
+    allowed_methods= ['POST', 'GET']
+    serializer_class = TechnologySerializer
+    queryset = Technology.objects.all()
+
+class TechnologyExperience(
+    RetrieveModelMixin,
+    ListModelMixin,
+    UpdateModelMixin,
+    GenericViewSet,
+    CreateModelMixin):
+    allowed_methods= ['POST', 'GET']
+    serializer_class = TechnologyExperience
+    queryset = TechnologyExperience.objects.all()
+    
