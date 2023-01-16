410682eebecdbad7d9b933e4802aca07bc7b9b74
DiazRock <corolariodiaz@gmail.com>
2023-01-16 08:45:45 -0500

    Added the views for check user experience in techs

--- crud/users/api/views.py
+++ crud/users/api/views.py
@@ -1,5 +1,6 @@
+from django.http.response import Http404
 from django.contrib.auth import get_user_model
-from rest_framework import status
+from rest_framework import status, Response
 from rest_framework.decorators import action
 from rest_framework.mixins import (
     ListModelMixin, 
@@ -34,6 +35,8 @@ class UserViewSet(
     def me(self, request):
         serializer = UserSerializer(request.user, context={"request": request})
         return Response(status=status.HTTP_200_OK, data=serializer.data)
+
+    
     
 
 class TechnologyViewSet(
@@ -46,13 +49,53 @@ class TechnologyViewSet(
     serializer_class = TechnologySerializer
     queryset = Technology.objects.all()
 
-class TechnologyExperience(
+class TechnologyExperienceViewSet(
     RetrieveModelMixin,
     ListModelMixin,
     UpdateModelMixin,
     GenericViewSet,
     CreateModelMixin):
     allowed_methods= ['POST', 'GET']
-    serializer_class = TechnologyExperience
+    serializer_class = TechnologyExperienceSerializer
     queryset = TechnologyExperience.objects.all()
     
+
+    @action(methods=['get'], detail=True,
+            url_path='get-techs-experience-by-user')
+    def get_techs_experience_by_user(self, request, user_id):
+        """ Get the experience of the user in all techs"""
+        technologies = TechnologyExperience.
+                    objects.
+                    filter(user = user_id).
+                    values('technology', 'experience')
+        
+        
+        return Response(
+            [
+                {
+                "tech_name": Technology.objects.get(pk=t['technology']).name, 
+                "experience": t['experience'] 
+                } for t in technologies
+            ]
+            )
+
+    @action(methods=['get'], detail=True,
+            url_path='get-user-experience-by-tech')
+    def get_user_experience_by_tech(self, request, tech_id):
+        """ Get the experience of the user in all techs"""
+        users_and_exp = TechnologyExperience.
+                    objects.
+                    filter(technology = tech_id).
+                    values('user', 'experience')
+        
+        
+        return Response(
+            [
+                {
+                "user_name": User.objects.get(pk=t['user']).name, 
+                "experience": t['experience'] 
+                } for t in users_and_exp
+            ]
+            )
+    
+  
