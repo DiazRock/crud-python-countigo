6679ed34af524b5e99159b0ae325b7825a981eca
DiazRock <corolariodiaz@gmail.com>
2023-01-17 21:16:09 -0500

    Added validator to avoid reinsertion

--- crud/users/api/serializers.py
+++ crud/users/api/serializers.py
@@ -19,6 +19,7 @@ class ApplicantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Applicant
         fields = [
+            "id",
             "first_name", 
             "last_name",
             "actual_address",
@@ -34,14 +35,24 @@ class TechnologySerializer(serializers.ModelSerializer):
     class Meta:
         model = Technology
         fields = [
-            "name"
+            "id",
+            "tech_name"
         ]
 
 class TechnologyExperienceSerializer(serializers.ModelSerializer):
     class Meta:
         model = TechnologyExperience
         fields = [
-            "user",
+            "applicant",
             "technology",
             "experience"
         ]
+    
+    def validate(self, data):
+        ret = super().validate(data)
+        technology = int (self.context['view'].request.data['technology'])
+        applicant = int (self.context['view'].request.data['applicant'])
+        if TechnologyExperience.objects.filter(applicant = applicant, technology = technology ):
+            raise serializers.ValidationError('Reinserting of user experience in tech is not allowed')
+        
+        return ret
